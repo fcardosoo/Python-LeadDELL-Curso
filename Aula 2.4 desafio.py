b85dd9523c13bfb98ebf604482d2e20b40b67a89
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-05-21 22:35:30 -0300

    Create Aula 2.4 desafio.py

---
+++ Aula 2.4 desafio.py
@@ -0,0 +1,27 @@
+'''
+Oficina Aula 2.4
+
+Durante a aula, foi apresentado a criação de dicionários em diferentes cenários. Então, chegou o momento de praticar, a partir do que você aprendeu, e utilizar dicionários, como estrutura básica, para resolver o seguinte problema: Imagine que você foi contratado para construir o sistema de gerenciamento de uma livraria e é o responsável por criar um módulo/célula, que fará a leitura dos dados de livros, que serão Título, gênero, subgênero, editora, número de cópias em loja e valor em reais. Além disso, você também é o responsável pelo módulo de apresentação dos livros disponíveis em loja, seguindo a seguinte organização: ao se apresentar todos os livros disponíveis, deve-se separá-los, primeiro, por gênero e, depois, por subgênero, seguindo a estrutura a seguir:
+
+--- Gênero A ---
+------ Subgênero A.1 ------
+Livro 01
+Livro 02
+------ Subgênero A.2 ------
+Livro 03
+--- Gênero B ---
+------ Subgênero B.1 ------
+Livro 04
+------ Subgênero B.2 ------
+Livro 05
+Livro 06
+
+Tanto gênero quanto subgênero devem seguir uma ordem
+alfabética de apresentação, em que, dentro de um
+subgênero, os livros devem ser listados pela quantidade
+disponível em loja. Vamos lá?
+'''
+
+# Prezado Alan, estou sem tempo para completar esta atividade hoje,
+# vou enviar estes ultimos desafios assim para poder concluir
+# o curso que encerra amanhã. Depois retornarei para faze-los.
